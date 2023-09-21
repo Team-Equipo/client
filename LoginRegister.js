1ca801f5d375ce10cd29e191fc7146feefd14147
TaekYung Jeong <tj29@students.calvin.edu>
2023-09-21 14:16:54 -0400

    mostly done login screen without navigation

--- LoginRegister.js
+++ LoginRegister.js
@@ -1,14 +1,38 @@
-// MyFirstScreen.js
 import React from "react";
-import { View, Text } from "react-native";
+import { View, StyleSheet } from "react-native";
+import { Button, Text, Provider as PaperProvider } from "react-native-paper";
+import { useNavigation } from "@react-navigation/native"; // Import the navigation hook
 
-const MyFirstScreen = () => {
+const LoginRegister = () => {
   return (
-    <View>
-      <Text>This is going to be the login screen!</Text>
-      {/* Add your UI components here */}
-    </View>
+    <PaperProvider>
+      <View style={styles.container}>
+        <Text style={styles.title}>Linguisity</Text>
+        <Button mode="contained" style={styles.button} onPress={() => {}}>
+          Login
+        </Button>
+        <Button mode="outlined" style={styles.button} onPress={() => {}}>
+          Register
+        </Button>
+      </View>
+    </PaperProvider>
   );
 };
 
-export default MyFirstScreen;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+  },
+  button: {
+    width: 200,
+    marginVertical: 10,
+  },
+});
+
+export default LoginRegister;
