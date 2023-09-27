4240550cd23a9d7f5b08fbb85dfc77bec6db4a5a
Cobblestone5517 <kjy11gold@gmail.com>
2023-09-26 20:35:25 -0400

    change icons: change main icon and add temporary OAuth icons

--- src/SignIn.js
+++ src/SignIn.js
@@ -7,8 +7,8 @@ import {
   View,
   Keyboard,
   TouchableWithoutFeedback,
+  Linking,
 } from "react-native";
-import { Provider as PaperProvider } from "react-native-paper";
 import {
   Appbar,
   Title,
@@ -17,7 +17,19 @@ import {
   Button,
   withTheme,
   Text,
+  DefaultTheme,
+  PaperProvider,
 } from "react-native-paper";
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: "blue", // Change this to your desired primary color
+    text: "black", // Change this to your desired text color
+    outline: "#51c3f5", // Change this to your desired outline color
+  },
+};
 
 const SignIn = () => {
   const [email, setEmail] = React.useState(null);
@@ -37,80 +49,143 @@ const SignIn = () => {
   const findPassword = () => {};
 
   return (
-    <DismissKeyboard>
-      <View>
-        <Appbar.Header>
-          <Appbar.Content
-            mode="center-aligned"
-            style={styles.topbar}
-            title="Lingucidity"
-          />
-        </Appbar.Header>
-        <View style={{ padding: 16 }}>
-          <Image
-            source={require("../assets/airplane.png")}
-            style={styles.image}
-          />
+    <PaperProvider theme={theme}>
+      <DismissKeyboard>
+        <View>
+          <Appbar.Header>
+            <Appbar.Content
+              mode="center-aligned"
+              type="large"
+              theme={{ fontWeight: "bold", colors: { primary: "black" } }}
+              style={styles.topbar}
+              title="Lingucidity"
+            />
+          </Appbar.Header>
+          <View style={{ padding: 16 }}>
+            <View style={styles.backArea}>
+              <Image
+                /*source={require("../assets/airplane.png")}*/
+                source={require("../assets/paperPlane.png")}
+                style={styles.image}
+              />
 
-          <Title style={styles.title}>Welcome Back!</Title>
-          <TextInput
-            mode="outlined"
-            label="Username/Email"
-            value={email}
-            onChangeText={(text) => setEmail(text)}
-            autoCapitalize="none"
-            autoCompleteType="email"
-            textContentType="emailAddress"
-            keyboardType="email-address"
-          />
-          <TextInput
-            mode="outlined"
-            label="Password"
-            value={password}
-            onChangeText={(text) => setPassword(text)}
-            secureTextEntry
-          />
+              <Title style={styles.title}>Welcome Back!</Title>
 
-          <View style={styles.forgotPassword}>
-            <TouchableOpacity onPress={findPassword}>
-              <Text style={styles.label}>Forgot your password?</Text>
-            </TouchableOpacity>
+              <TextInput
+                mode="outlined"
+                label="Username"
+                value={email}
+                onChangeText={(text) => setEmail(text)}
+                autoCapitalize="none"
+                autoCompleteType="email"
+                textContentType="emailAddress"
+                keyboardType="email-address"
+              />
+              <TextInput
+                mode="outlined"
+                label="Password"
+                value={password}
+                onChangeText={(text) => setPassword(text)}
+                secureTextEntry
+              />
+
+              <View style={styles.forgotPassword}>
+                <TouchableOpacity onPress={findPassword}>
+                  <Text style={styles.label}>Forgot your password?</Text>
+                </TouchableOpacity>
+              </View>
+
+              <Button
+                style={styles.signInButton}
+                theme={{ fontWeight: "bold", colors: { primary: "white" } }}
+                mode="elevated"
+                onPress={handleSignIn}
+              >
+                Sign In
+              </Button>
+
+              <View style={styles.row}>
+                <Text style={styles.label}>Don’t have an account? </Text>
+                <TouchableOpacity onPress={createAccount}>
+                  <Text style={styles.link}>Sign up</Text>
+                </TouchableOpacity>
+              </View>
+
+              <View style={styles.oauthContainer}>
+                <View style={styles.oauthImageContainer}>
+                  <TouchableOpacity
+                    onPress={() => {
+                      Linking.openURL(
+                        "https://www.google.com/",
+                      ); /* Replate the link by Google OAuth link */
+                    }}
+                  >
+                    <Image
+                      source={require("../assets/google.png")}
+                      resizeMode="contain"
+                      style={styles.oauthImage}
+                    />
+                  </TouchableOpacity>
+                </View>
+                <View style={styles.oauthImageContainer}>
+                  <TouchableOpacity
+                    onPress={() => {
+                      Linking.openURL(
+                        "https://www.facebook.com/",
+                      ); /* Replate the link by Facebook OAuth link */
+                    }}
+                  >
+                    <Image
+                      source={require("../assets/facebook.png")}
+                      resizeMode="contain"
+                      style={styles.oauthImage}
+                    />
+                  </TouchableOpacity>
+                </View>
+                <View style={styles.oauthImageContainer}>
+                  <TouchableOpacity
+                    onPress={() => {
+                      Linking.openURL(
+                        "https://www.microsoft.com/",
+                      ); /* Replate the link by Microsoft OAuth link */
+                    }}
+                  >
+                    <Image
+                      source={require("../assets/microsoft.png")}
+                      resizeMode="contain"
+                      style={styles.oauthImage}
+                    />
+                  </TouchableOpacity>
+                </View>
+              </View>
+            </View>
           </View>
 
-          <Button
-            style={styles.signInButton}
-            theme={{ fontWeight: "bold", colors: { primary: "white" } }}
-            mode="elevated"
-            onPress={handleSignIn}
+          <Snackbar
+            visible={snackbarVisible}
+            onDismiss={() => setSnackbarVisible(false)}
           >
-            Sign In
-          </Button>
-
-          <View style={styles.row}>
-            <Text style={styles.label}>Don’t have an account? </Text>
-            <TouchableOpacity onPress={createAccount}>
-              <Text style={styles.link}>Sign up</Text>
-            </TouchableOpacity>
-          </View>
+            Sign In Successful!
+          </Snackbar>
         </View>
-        <Snackbar
-          visible={snackbarVisible}
-          onDismiss={() => setSnackbarVisible(false)}
-        >
-          Sign In Successful!
-        </Snackbar>
-      </View>
-    </DismissKeyboard>
+      </DismissKeyboard>
+    </PaperProvider>
   );
 };
 
 const styles = StyleSheet.create({
   image: {
-    width: 256,
-    height: 256,
+    width: 128,
+    height: 128,
     marginTop: 12,
-    marginBottom: 12,
+    marginBottom: 32,
     alignSelf: "center", // Center the image horizontally
+  },
+  backArea: {
+    backgroundColor: "#dbf4ff",
+    borderRadius: 16,
+    padding: 24,
+    marginTop: 24,
   },
   topbar: {
     color: "black",
@@ -143,6 +218,29 @@ const styles = StyleSheet.create({
   signInButton: {
     justifyContent: "space-around",
     backgroundColor: "blue",
+    marginVertical: 5,
+  },
+  oauthContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  oauthImageContainer: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white",
+    alignSelf: "flex-start",
+    marginHorizontal: 15,
+    marginTop: 10,
+    padding: 8,
+    borderRadius: 32,
+  },
+  oauthImage: {
+    width: 48,
+    height: 48,
+    borderRadius: 24,
   },
 });
 
