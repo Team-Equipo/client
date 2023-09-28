bf1e2d901a142d48bcefa729b3183cff3bbb92c3
Jason Chew <jasonchew@Jasons-MacBook-Air-3.local>
2023-09-28 16:46:41 -0400

    Added Trip Info screen, put app header in own file

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
-import { PaperProvider } from "react-native-paper";
+import { PaperProvider, Appbar } from "react-native-paper";
 import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { View } from "react-native";
 import Main from "./src/Main";
-import Registration from "./src/Registration";
+import UserInfo from "./src/UserInfo";
+import TripInfo from "./src/TripInfo";
+import RegistrationBar from "./src/RegistrationBar";
+
+const Stack = createNativeStackNavigator();
 
 /**
  * Renders the main component of the application.
@@ -13,7 +19,25 @@ export default function App() {
   return (
     // Use Material 3 theme.
     <PaperProvider theme={{ version: 3 }}>
-      <Registration />
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="UserInfo"
+          screenOptions={{
+            header: (props) => <RegistrationBar {...props} />,
+          }}
+        >
+          <Stack.Screen
+            name="UserInfo"
+            component={UserInfo}
+            options={{ title: "Your Profile" }}
+          />
+          <Stack.Screen
+            name="TripInfo"
+            component={TripInfo}
+            options={{ title: "Your Trip" }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     </PaperProvider>
   );
 }
