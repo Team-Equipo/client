320e1327e2c9e877ac938c84deadcaa1982bc657
Jason Chew <jasonchew@Jasons-MacBook-Air-3.local>
2023-10-04 17:30:27 -0400

    Renamed src folder to screens

---
+++ screens/Main.js
@@ -0,0 +1,27 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import { Button } from "react-native-paper";
+
+export default function Main() {
+  return (
+    <View style={styles.container}>
+      <Button
+        icon="translate"
+        mode="contained"
+        onPress={() => console.log("Pressed")}
+      >
+        Lingucidity
+      </Button>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
