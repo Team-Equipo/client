072e7e4c91bb9d33391d4dd3b2d7dc5163ee4a37
Jiho Kim <55632840+nghtctrl@users.noreply.github.com>
2023-10-06 01:24:30 -0400

    Use primary color from default theme

--- components/AppBar.js
+++ components/AppBar.js
@@ -1,13 +1,14 @@
 import { getHeaderTitle } from "@react-navigation/elements";
 import * as React from "react";
-import { Appbar, Menu } from "react-native-paper";
+import { Appbar, useTheme } from "react-native-paper";
 
 export default function AppBar({ route, options }) {
   const title = getHeaderTitle(options, route.name);
+  const theme = useTheme();
 
   if (route.name === "Home") {
     return (
-      <Appbar.Header style={{ backgroundColor: "#824f8d" }}>
+      <Appbar.Header style={{ backgroundColor: theme.colors.primary }}>
         <Appbar.Action
           icon="menu"
           size={33}
@@ -29,7 +30,7 @@ export default function AppBar({ route, options }) {
     );
   } else {
     return (
-      <Appbar.Header style={{ backgroundColor: "#824f8d" }}>
+      <Appbar.Header style={{ backgroundColor: theme.colors.primary }}>
         <Appbar.Content title={title} />
       </Appbar.Header>
     );
