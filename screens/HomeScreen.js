8f518ffd7e33812e776b8424829d4f10709ee529
Geesing <chewgeesing@gmail.com>
2023-11-09 17:37:37 -0500

    Changed to bottom tab navigation

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,50 +1,100 @@
-import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import React, { useState } from "react";
-import { Button, withTheme, useTheme, Text } from "react-native-paper";
+import {
+  BottomNavigation,
+  Button,
+  withTheme,
+  useTheme,
+  Text,
+} from "react-native-paper";
+import { CommonActions } from "@react-navigation/native";
 
 import Phrases from "./Phrases";
 import SavedPhrases from "./SavedPhrases";
 import Translation from "./Translation";
 import { signinTheme } from "../styles/globalStyles";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
-const Tab = createMaterialTopTabNavigator();
+const Tab = createBottomTabNavigator();
 
 const HomeScreen = ({ navigation, route, options, back }) => {
   const theme = useTheme();
   return (
     <Tab.Navigator
-      initialRouteName="Find Phrases"
       screenOptions={{
-        tabBarIndicatorContainerStyle: {
-          padding: 0,
-          marginTop: 0,
-        },
-        tabBarStyle: {
-          marginTop: -10,
-          backgroundColor: signinTheme.colors.primary,
-        },
-        tabBarIndicatorStyle: {
-          backgroundColor: "darkblue",
-          height: 3,
-        },
-        tabBarActiveTintColor: "white",
-        swipeEnabled: false,
+        headerShown: false,
       }}
+      tabBar={({ navigation, state, descriptors, insets }) => (
+        <BottomNavigation.Bar
+          navigationState={state}
+          safeAreaInsets={insets}
+          onTabPress={({ route, preventDefault }) => {
+            const event = navigation.emit({
+              type: "tabPress",
+              target: route.key,
+              canPreventDefault: true,
+            });
+
+            if (event.defaultPrevented) {
+              preventDefault();
+            } else {
+              navigation.dispatch({
+                ...CommonActions.navigate(route.name, route.params),
+                target: state.key,
+              });
+            }
+          }}
+          renderIcon={({ route, focused, color }) => {
+            const { options } = descriptors[route.key];
+            if (options.tabBarIcon) {
+              return options.tabBarIcon({ focused, color, size: 24 });
+            }
+
+            return null;
+          }}
+          getLabelText={({ route }) => {
+            const { options } = descriptors[route.key];
+            const label =
+              options.tabBarLabel !== undefined
+                ? options.tabBarLabel
+                : options.title !== undefined
+                ? options.title
+                : route.title;
+
+            return label;
+          }}
+        />
+      )}
     >
       <Tab.Screen
         name="Find Phrases"
         component={Phrases}
-        options={{ tabBarLabel: "Find Phrases" }}
+        options={{
+          tabBarLabel: "Find Phrases",
+          tabBarIcon: () => {
+            return <Icon name="archive-search" size={25} />;
+          },
+        }}
       />
       <Tab.Screen
         name="Translation"
         component={Translation}
-        options={{ tabBarLabel: "Translation" }}
+        options={{
+          tabBarLabel: "Translation",
+          tabBarIcon: () => {
+            return <Icon name="translate" size={25} />;
+          },
+        }}
       />
       <Tab.Screen
         name="SavedPhrases"
         component={SavedPhrases}
-        options={{ tabBarLabel: "Saved" }}
+        options={{
+          tabBarLabel: "Saved",
+          tabBarIcon: () => {
+            return <Icon name="bookmark" size={25} />;
+          },
+        }}
       />
     </Tab.Navigator>
   );
