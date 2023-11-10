6408f647b1566c3bced5c555886a49bbdaed8f4f
Jiho Kim <55632840+nghtctrl@users.noreply.github.com>
2023-11-09 19:18:00 -0500

    Revert "Merge pull request #30 from Team-Equipo/newSplash/ChrisJeong2383"
    This reverts commit ff2a4914352e9e45913707313131db216f2a5b35, reversing
    changes made to 4aeef6b42d1193c050ae8b5352cb2b0bd8928668.

--- screens/SplashScreen.js
+++ screens/SplashScreen.js
@@ -1,28 +1,10 @@
 import React from "react";
-import { Text, View, Image, StyleSheet } from "react-native";
-import logo from "../assets/splashlogo.png";
+import { Text, View } from "react-native";
+
 export default function SplashScreen() {
   return (
-    <View style={styles.container}>
-      <Image source={logo} style={styles.image} />
+    <View>
+      <Text>Loading...</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#FFFFFF",
-  },
-  image: {
-    width: 150,
-    height: 150,
-    marginBottom: "5%",
-  },
-  text: {
-    marginTop: 10,
-    fontSize: 20,
-  },
-});
