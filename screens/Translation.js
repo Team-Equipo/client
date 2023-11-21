4489a1238da70cd6ba9c27082a4dc5fe8f2fe566
Geesing <chewgeesing@gmail.com>
2023-11-21 09:35:49 -0500

    Refactored language toggling to actually swap rather than using hard-coded values, improved function names, replaced real-time translation with Submit button, changed translation function to use ISO code of current outputLang

--- screens/Translation.js
+++ screens/Translation.js
@@ -10,11 +10,9 @@ import {
   TouchableOpacity,
   useWindowDimensions,
 } from "react-native";
-import DropDownPicker from "react-native-dropdown-picker";
 import {
-  Divider,
-  IconButton,
   Button,
+  IconButton,
   withTheme,
   Portal,
   Modal,
@@ -31,26 +29,29 @@ import {
 } from "../styles/globalStyles";
 
 const Translation = ({ navigation }) => {
-  const [textToTranslate, setTextToTranslate] = React.useState("");
-  const [translation, setTranslation] = React.useState("");
+  // States for I/O, dictionary loading and displaying
   const [inputLang, setInputLang] = React.useState("English");
   const [outputLang, setOutputLang] = React.useState("Spanish");
-  const [selectedWord, setSelectedWord] = React.useState("");
-  const [wordRefURL, setWordRefURL] = React.useState(
+  const [translationInput, setTranslationInput] = React.useState("");
+  const [translationOutput, setTranslationOutput] = React.useState("");
+  const [searchedWord, setSearchedWord] = React.useState("");
+  const [wordRefEndpoint, setWordRefEndpoint] = React.useState(
     "https://www.wordreference.com/es/en/translation.asp?spen=",
   );
   const [wordRefLoading, setWordRefLoading] = React.useState(false);
+  const [wordRefVisible, setWordRefVisible] = useState(false);
 
-  const [modalVisible, setModalVisible] = useState(false);
+  // To encode/decode ISO language codes used by Microsoft Translate
+  const ISO6391 = require("iso-639-1");
 
-  useEffect(() => {
-    handleTranslationText(textToTranslate, inputLang);
-  }, [inputLang, textToTranslate]);
-
+  // Function to send text and target language to Microsoft Translator API,
+  // return received translation or error if something went wrong
   const translateText = async (text, targetLanguage) => {
+    // apiKey stored in environmental variable to avoid exposure in Github
     const apiKey = process.env.EXPO_PUBLIC_TRANSLATION_KEY;
     const endpoint = `https://api.cognitive.microsofttranslator.com/translate?api-version=3.0&to=${targetLanguage}`;
 
+    // Fetch translation
     try {
       const response = await fetch(endpoint, {
         method: "POST",
@@ -63,8 +64,8 @@ const Translation = ({ navigation }) => {
         body: JSON.stringify([{ text }]),
       });
 
+      // Handle HTTP errors
       if (!response.ok) {
-        // Handle non-successful responses (e.g., HTTP error status)
         throw new Error(
           `Translation request failed with status ${response.status}`,
         );
@@ -72,11 +73,13 @@ const Translation = ({ navigation }) => {
 
       const data = await response.json();
 
+      // Ensure data, data.translations, and data.translations.text contain
+      // something
       if (!data || !data[0]?.translations || !data[0].translations[0]?.text) {
-        // Handle unexpected response structure
         throw new Error("Unexpected response format from translation API");
       }
 
+      // Return the successful translation if all goes well
       return data[0].translations[0].text;
     } catch (error) {
       // Handle other errors (e.g., network issues, parsing errors)
@@ -85,59 +88,54 @@ const Translation = ({ navigation }) => {
     }
   };
 
-  const handleTranslationText = async (text) => {
-    // console.log(process.env.EXPO_PUBLIC_FAKE_KEY);
+  // Function to request translation in target language, set it as output
+  // (sets empty string output if input is empty)
+  const doTranslation = async (text) => {
     if (text != "") {
-      setTextToTranslate(text);
-      const microsoftTranslation = await translateText(text, "es");
-      console.log(microsoftTranslation);
-      setTranslation(microsoftTranslation);
+      const microsoftTranslation = await translateText(
+        text,
+        ISO6391.getCode(outputLang),
+      );
+      setTranslationOutput(microsoftTranslation);
+    } else {
+      setTranslationOutput("");
     }
-
-    // if (text == "") {
-    //   setTranslation("");
-    // } else if (inputLang == "English") {
-    //   if (text == "Hello world") {
-    //     setTranslation("Hola mundo");
-    //   } else {
-    //     setTranslation("Esto es una respuesta genérica...");
-    //   }
-    // } else if (inputLang == "Spanish") {
-    //   if (text == "Hola mundo") {
-    //     setTranslation("Hello world");
-    //   } else {
-    //     setTranslation("This is a generic response...");
-    //   }
-    // }
   };
 
+  // Function to switch input and output languages and their corresponding
+  // Wordreference endpoints
   const toggleLang = () => {
+    var temp = inputLang;
+    setInputLang(outputLang);
+    setOutputLang(temp);
+
+    temp = translationInput;
+    setTranslationInput(translationOutput);
+    setTranslationOutput(temp);
+
     if (inputLang == "English") {
-      setInputLang("Spanish");
-      setOutputLang("English");
-      setWordRefURL(
+      setWordRefEndpoint(
         "https://www.wordreference.com/es/translation.asp?tranword=",
       );
     } else if (inputLang == "Spanish") {
-      setInputLang("English");
-      setOutputLang("Spanish");
-      setWordRefURL(
+      setWordRefEndpoint(
         "https://www.wordreference.com/es/en/translation.asp?spen=",
       );
     }
-    handleTranslationText(textToTranslate);
   };
 
   function showModal(item) {
-    setModalVisible(true);
+    setWordRefVisible(true);
   }
 
   function hideModal() {
-    setModalVisible(false);
+    setWordRefVisible(false);
   }
 
+  // Function to filter punctuation out of selected word, pull up dictionary
+  // modal for selected word
   function selectWord(word) {
-    setSelectedWord(word.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, ""));
+    setSearchedWord(word.replace(/[¡!"#$%&'()*+,-./:;<=>¿?@[\]^_`{|}~]/g, ""));
     showModal();
   }
 
@@ -148,8 +146,8 @@ const Translation = ({ navigation }) => {
           <SafeAreaView>
             <Portal>
               <Modal
-                visible={modalVisible}
-                onDismiss={() => setModalVisible(false)}
+                visible={wordRefVisible}
+                onDismiss={() => setWordRefVisible(false)}
                 contentContainerStyle={translateStyles.modalContainer}
               >
                 <View
@@ -172,7 +170,7 @@ const Translation = ({ navigation }) => {
                   ) : null}
                   <WebView
                     originWhitelist={["*"]}
-                    source={{ uri: wordRefURL + selectedWord }}
+                    source={{ uri: wordRefEndpoint + searchedWord }}
                     onLoadStart={() => setWordRefLoading(true)}
                     onLoadProgress={() => setWordRefLoading(false)}
                     style={{ borderRadius: 15, marginTop: -180 }}
@@ -197,17 +195,13 @@ const Translation = ({ navigation }) => {
                   marginRight: 10,
                   zIndex: 3,
                   alignItems: "flex-end",
-                  // marginLeft: "30%",
-                  // marginRight: "30%",
                 }}
               >
                 <IconButton
                   onPress={toggleLang}
                   icon="swap-vertical"
                   iconColor="gray"
-                  // compact={true}
                   style={{ width: 25, height: 30, marginTop: 4 }}
-                  // labelStyle={{fontSize: 30, color: "gray", fontWeight: "normal"}}
                 ></IconButton>
               </View>
               <View
@@ -215,7 +209,6 @@ const Translation = ({ navigation }) => {
                   flexDirection: "row",
                   justifyContent: "flex-start",
                   zIndex: 2,
-                  //marginTop: 15,
                 }}
               >
                 <Text style={translateStyles.languageLabel}>{inputLang}</Text>
@@ -225,17 +218,12 @@ const Translation = ({ navigation }) => {
                   style={{
                     justifyContent: "flex-start",
                     textAlignVertical: "top",
-                    paddingTop: 0,
                     height: "100%",
                     fontSize: 20,
-                    //backgroundColor: "blue",
-                    marginTop: 0,
                   }}
                   multiline
-                  value={textToTranslate}
-                  onChangeText={(text) =>
-                    handleTranslationText(text, inputLang)
-                  }
+                  value={translationInput}
+                  onChangeText={(text) => setTranslationInput(text)}
                 />
               </View>
               <View
@@ -244,7 +232,6 @@ const Translation = ({ navigation }) => {
                   marginRight: 4,
                   flexDirection: "row",
                   justifyContent: "space-between",
-                  // backgroundColor: "purple",
                 }}
               >
                 <Text style={{ ...translateStyles.languageLabel }}>
@@ -257,9 +244,9 @@ const Translation = ({ navigation }) => {
                   mode="default"
                   onPress={() => {
                     if (outputLang === "Spanish") {
-                      Speech.speak(translation, { language: "es" });
+                      Speech.speak(translationOutput, { language: "es" });
                     } else if (outputLang === "English") {
-                      Speech.speak(translation, { language: "en" });
+                      Speech.speak(translationOutput, { language: "en" });
                     }
                   }}
                 />
@@ -278,7 +265,7 @@ const Translation = ({ navigation }) => {
                   columnWrapperStyle={{
                     flexWrap: "wrap",
                   }}
-                  data={translation.split(" ")}
+                  data={translationOutput.split(" ")}
                   renderItem={({ item }) => (
                     <TouchableOpacity
                       onPress={() => {
@@ -304,6 +291,11 @@ const Translation = ({ navigation }) => {
               >
                 Tap a translated word for the dictionary definition
               </Text>
+              <Button
+                onPress={() => doTranslation(translationInput, outputLang)}
+              >
+                Submit
+              </Button>
             </View>
           </SafeAreaView>
         </View>
