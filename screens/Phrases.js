1e1f51b90652805b0cbd28dbbaa8e89c45cafc57
Geesing <chewgeesing@gmail.com>
2023-11-26 11:33:25 -0500

    Added sample phrases substituting for those from our data service

--- screens/Phrases.js
+++ screens/Phrases.js
@@ -41,6 +41,24 @@ const Phrases = ({ navigation }) => {
   const [wordRefVisible, setWordRefVisible] = useState(false);
   const { storageChange, setStorageChange } = usePhraseStorageTracker();
 
+  const samplePhrases = [
+    {
+      generated_phrases_id: 1,
+      userid: 123,
+      text_translated: "Hello, how are you?",
+      text_original: "Hola, ¿cómo estás?",
+      isloading: false,
+    },
+    {
+      generated_phrases_id: 2,
+      userid: 456,
+      text_translated: "Good morning!",
+      text_original: "¡Buenos días!",
+      isloading: false,
+    },
+    // Add more test phrases as needed
+  ];
+
   // Function to filter punctuation out of selected English word, pull up
   // dictionary modal for selected word
   const selectEnglishWord = (word, inputLang) => {
@@ -281,7 +299,7 @@ const Phrases = ({ navigation }) => {
               {/* The list of PhraseCards */}
               <FlatList
                 contentContainerStyle={{ alignItems: "center", rowGap: 8 }}
-                data={generatedPhrases}
+                data={samplePhrases}
                 renderItem={({ item }) => (
                   <PhraseCard
                     phrase={item}
