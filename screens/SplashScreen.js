1161045a64901434119cfc0e2a4db9dc1ddacaa5
TaekYung Jeong <tj29@students.calvin.edu>
2023-11-27 01:50:27 -0500

    splash

--- screens/SplashScreen.js
+++ screens/SplashScreen.js
@@ -1,10 +1,28 @@
 import React from "react";
-import { Text, View } from "react-native";
-
+import { Text, View, Image, StyleSheet } from "react-native";
+import logo from "../assets/splashlogo.png";
 export default function SplashScreen() {
   return (
-    <View>
-      <Text>Loading...</Text>
+    <View style={styles.container}>
+      <Image source={logo} style={styles.image} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#FFFFFF",
+  },
+  image: {
+    width: 150,
+    height: 150,
+    marginBottom: "5%",
+  },
+  text: {
+    marginTop: 10,
+    fontSize: 20,
+  },
+});
